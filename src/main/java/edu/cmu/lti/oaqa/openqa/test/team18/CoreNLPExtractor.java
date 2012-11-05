a485a68e4daf0987755f83cf7b51ab8d8eb2c929
mimighost <taylortianyuchen@gmail.com>
2012-11-05 17:30:27 -0500

    Add Stanford CoreNLP code

---
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team18/CoreNLPExtractor.java
@@ -0,0 +1,16 @@
+package edu.cmu.lti.oaqa.openqa.test.team18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
+import edu.cmu.lti.oaqa.framework.data.Keyterm;
+
+
+
+public class CoreNLPExtractor {
+
+}
