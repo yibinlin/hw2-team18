5fd8ca8e9f62bb1d9079e5a655bdc4b896d2e1a0
yibinl <lyyb46@gmail.com>
2012-11-21 16:19:17 -0500

    writing syntax parsing methods.

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team18/keyterm/SyntaxParsing.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team18/keyterm/SyntaxParsing.java
@@ -15,22 +15,58 @@ import edu.stanford.nlp.util.CoreMap;
 
 public class SyntaxParsing {
   private StanfordCoreNLP pipeline;
+  List<String> candidates;
 
   public SyntaxParsing() throws ResourceInitializationException {
     Properties props = new Properties();
     props.put("annotators", "tokenize, ssplit, pos, parse");
     pipeline = new StanfordCoreNLP(props);
+    candidates = new LinkedList<String>();
   }
 
   public List<String> getKeytermCandidates(String text) {
-    List<String> candidates = new LinkedList<String>();
+    
     Annotation document = new Annotation(text);
     pipeline.annotate(document);
     List<CoreMap> sentences = document.get(SentencesAnnotation.class);
     for (CoreMap sentence : sentences) {
-      Tree tree = sentence.get(TreeAnnotation.class);
-      tree.pennPrint();
+      Tree root = sentence.get(TreeAnnotation.class);
+      Tree cNode = root;
+      goThroughTree(root);
+      System.out.println(root.label().value());
     }
     return candidates;
+  }
+  
+  private synchronized void addCandidate(String phrase)
+  {
+      candidates.add(phrase);
+  }
+
+  private void goThroughTree(Tree node) {
+    if(node == null)
+    {
+      return;
+    }else
+    {
+      System.out.println(node.label().value());
+      Tree[] children = node.children();
+      for(Tree child : children)
+      {
+        goThroughTree(child);
+      }
+    }
+  }
+  
+  /**
+   * update the candidates List for some part of the tree.
+   * 
+   * @param node the node of the tree. The method cancatenates 
+   * all the words that was covered by the node and put it under 
+   * the add to the candidates. 
+   */
+  private void updateCandidate(Tree node)
+  {
+    
   }
 }
